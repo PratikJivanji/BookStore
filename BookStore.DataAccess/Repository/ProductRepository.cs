b1ebb2c5e38df095e34e1430dce791cca3f51f4e
PratikJivanji <pratik.l.jivanji@gmail.com>
2022-03-23 03:26:04 +1300

    added products and home page

---
+++ BookStore.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.DataAcesss;
+using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u=>u.Id==obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Author = obj.Author;
+                objFromDb.Price = obj.Price;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.CoverTypeId = obj.CoverTypeId;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
---
+++ BookStore.Models/Product.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        [ValidateNever]
+        public Category Category { get; set; }
+
+        [Required]
+        [Display(Name = "Cover Type")]
+        public int CoverTypeId { get; set; }
+        [ValidateNever]
+        public CoverType CoverType { get; set; }
+    }
+}
